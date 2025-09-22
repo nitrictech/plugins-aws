76b189a2b94f572a9bd05108d8be76d8d1c8266b
Tim Holm <timothy.j.holm@gmail.com>
2025-09-22 13:34:05 +1000

    fix typo.

--- loadbalancer/module/main.tf
+++ loadbalancer/module/main.tf
@@ -35,7 +35,7 @@ resource "aws_lb_listener" "http" {
 
 # Allow HTTP traffic from internet
 resource "aws_security_group_rule" "http_ingress" {
-  count             = count(var.security_groups)
+  count             = length(var.security_groups)
   security_group_id = var.security_groups[count.index]
   from_port         = local.listener_port
   to_port           = local.listener_port
@@ -46,7 +46,7 @@ resource "aws_security_group_rule" "http_ingress" {
 
 # Allow HTTP traffic from specified prefix lists
 resource "aws_security_group_rule" "prefix_list_ingress" {
-  count          = count(var.security_groups)
+  count          = length(var.security_groups)
   security_group_id = var.security_groups[count.index]
   from_port         = local.listener_port
   to_port           = local.listener_port
