a561ec7f7ce8704145ea4978563b32a21c3c298e
Rak Siva <rak.siva@nitric.io>
2025-09-30 15:01:37 -0600

    fix: ensure unique Lambda Edge role name

--- cloudfront/module/main.tf
+++ cloudfront/module/main.tf
@@ -138,7 +138,7 @@ resource "aws_cloudfront_function" "api-url-rewrite-function" {
 # Lambda@Edge function for auth preservation and webhook signing
 resource "aws_iam_role" "lambda_edge_origin_request" {
   count = length(local.lambda_origins) > 0 ? 1 : 0
-  name  = "lambda-edge-origin-request-role"
+  name  = "${var.suga.stack_id}-lambda-edge-origin-request-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
