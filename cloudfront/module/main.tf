03d8257e6cb4750e2a85edc4fbd2ff04912dba35
Tim Holm <timothy.j.holm@gmail.com>
2025-10-20 12:01:39 +1100

    fix: ensure default created cache policy is unique to stack.

--- cloudfront/module/main.tf
+++ cloudfront/module/main.tf
@@ -338,7 +338,7 @@ resource "aws_acm_certificate" "cloudfront_cert" {
 
 resource "aws_cloudfront_cache_policy" "default_cache_policy" {
   count   = var.default_cache_policy_id == null ? 1 : 0
-  name    = "SugaDefaultCachePolicy"
+  name    = "SugaDefaultCachePolicy-${var.suga.stack_id}"
   comment = "Default cache policy for CloudFront distribution for Suga Applications"
 
   # Set TTL defaults to respect cache control headers
